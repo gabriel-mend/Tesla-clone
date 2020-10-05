f1c8c50254f872cd74da9732ee4926dd3743120f
Gabriel Mendonca <gabrielmendoncapereira14@gmail.com>
2020-10-05 11:28:32 -0300

    Homepage tesla clone

---
+++ src/components/UniqueOverlay/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useTransform } from 'framer-motion';
+import useWrapperScroll from '../Model/useWrapperScroll';
+
+import { 
+  Container,
+  Header,
+  Logo,
+  Burger,
+  Footer
+} from './styles';
+
+const UniqueOverlay: React.FC = () => {
+  const { scrollYProgress } = useWrapperScroll();
+
+  const opacity = useTransform(scrollYProgress, [0.9, 1], [0, 1])
+  
+  return (
+    <Container>
+      <Header>
+        <Logo />
+        <Burger />
+      </Header>
+      <Footer style={{opacity}}>
+        <ul>
+          <li>
+            <a href="">Ui clone</a>
+          </li>
+          <li>
+            <a href="">made with 💜</a>
+          </li>
+          <li>
+            <a href="">by Gabriel Mendonça</a>
+          </li>
+        </ul>
+      </Footer>
+    </Container>
+  );
+};
+
+export default UniqueOverlay;
