555047d84c50a329454b422139f981e3ba11b6a4
Gabriel Mendonca <gabrielmendoncapereira14@gmail.com>
2020-10-09 18:29:12 -0300

    style sections

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    .colored {
+        background-size: cover !important;
+        background-position: center !important;
+    }
     .colored:nth-child(1) {
         background: url('https://images.hdqwalls.com/download/tesla-roadster-2020-3p-1920x1080.jpg');
-        background-size: cover;
     }
     .colored:nth-child(2) {
-        background: url('http://wallpaperswide.com/download/tesla_model_x_suv_electric_car___white_sunset-wallpaper-1920x1080.jpg');
+        background: url('https://images.hdqwalls.com/download/tesla-model-x-front-4k-5x-1920x1080.jpg');
     }
     .colored:nth-child(3) {
         background: url('https://www.wattson.pt/wp-content/uploads/2019/08/952152.jpg');
-        background-size: cover;
     }
     .colored:nth-child(4) {
         background: url('https://images.hdqwalls.com/download/2018-tesla-roadster-side-view-4k-e4-1920x1080.jpg');
@@ -20,5 +22,5 @@ export const Container = styled.div`
 export const Spacer = styled.div`
     height: 15vh;
     background: url('https://images.hdqwalls.com/download/2018-tesla-roadster-side-view-4k-e4-1920x1080.jpg');
-    background-position: bottom
+    background-position: bottom;
 `;
